60be7dca5b7b1af3bc86cdd2f287a17070d344f3
Marek Kočvara <kocvara.mark@gmail.com>
2024-01-08 15:57:43 +0100

    ClientApp initial upload

---
+++ ClientApp/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClientApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+        }
+
+        public IActionResult OnGet()
+        {
+            if (/* TODO: User is logged in */ true)
+                return LocalRedirect("/dashboard");
+            else
+                return LocalRedirect("/login");
+        }
+    }
+}
