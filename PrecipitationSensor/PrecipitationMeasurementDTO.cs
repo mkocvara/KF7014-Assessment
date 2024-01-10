22324c55fa44fdea1df49529f88074828975f0fc
Marek Kočvara <kocvara.mark@gmail.com>
2024-01-10 18:30:22 +0100

    Fix test data in Precipitation sensors

--- PrecipitationSensor/PrecipitationMeasurementDTO.cs
+++ PrecipitationSensor/PrecipitationMeasurementDTO.cs
@@ -33,10 +33,10 @@ namespace PrecipitationSensors
 
             // random number generator
             System.Random random = new System.Random();
-            float randPrecipitationMm = (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble()) % 125.0);
+            float randPrecipitationMm = (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble()) * 125.0);
             float randCoverage = (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble()) * 100.0);
-            float randSnowfall = (random.NextInt64() % 8 == 0) ? (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble()) % 1.0) : 0.0f;
-            _testSnowDepth = Math.Max(0, _testSnowDepth + randSnowfall - (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble()) % 1.7f));
+            float randSnowfall = (random.NextInt64() % 8 == 0) ? (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble())) : 0.0f;
+            _testSnowDepth = Math.Max(0, _testSnowDepth + randSnowfall - (float)truncTwo(Math.Min(random.NextDouble(), random.NextDouble()) * 1.7f));
 
             // torrential rain
             if (_testCounter == 5)
