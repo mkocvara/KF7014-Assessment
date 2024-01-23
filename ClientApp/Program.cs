077d5b51798a6dc7e24dd47ff842870fb75d9060
Cosmin Bianu <cosmin.bianu200@gmail.com>
2024-01-23 14:22:12 +0200

    Added RabbitMQ server to Docker Orchestration and adapted code for ClientApp to use environment variables.

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddHttpClient("Gateway", client =>
 {
     // client.BaseAddress = new Uri("https://localhost:7081");
-    client.BaseAddress = new Uri("http://gateway");
+    client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("GATEWAY_URI")??"https://localhost:7081");
     return;
 });
 
@@ -20,7 +20,7 @@ builder.Services.AddSingleton<IReadOnlyRepository<TemperatureMeasurement>, Tempe
 builder.Services.AddSingleton<IReadOnlyRepository<WeatherMeasurement>, AggregateRepository>();
 
 // > Register event bus
-builder.Services.AddSingleton<IBus>(RabbitHutch.CreateBus("host=localhost"));
+builder.Services.AddSingleton<IBus>(RabbitHutch.CreateBus($"host={Environment.GetEnvironmentVariable("RABBITMQ_HOST")??"localhost"}"));
 
 var app = builder.Build();
 
